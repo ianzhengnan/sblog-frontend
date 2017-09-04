10ecc6e6a61d4cbb95f2a41a0da500e584c11849
ianzheng <flks_2005@qq.com>
2017-09-04 20:06:45 +0800

    add vue router

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+
+const Home = { template: '<div>This is Home.</div>'}
+const Foo = { template: '<div>This is Foo.</div>'}
+const Bar = { template: '<div>This is Bar.</div>'}
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {path: '/', name: 'home', component: Home},
+        {path: '/foo', name: 'foo', component: Foo},
+        {path: '/bar', name: 'bar', component: Bar}
+    ]
+})
+
+new Vue({
+    router,
+    template: `
+        <div id="app">
+            <h1>Named Routes</h1>
+            <p>Current route name: {{ $route.name }}</p>
+            <ul>
+                <li><router-link :to="{ name: 'home' }">home</router-link></li>
+                <li><router-link :to="{ name: 'foo' }">foo</router-link></li>
+                <li><router-link :to="{ name: 'bar', params: { id: 123 }}">bar</router-link></li>
+            </ul>
+            <router-view class="view"></router-view>
+        </div>
+    `
+}).$mount('#app')
