b94ee3ca00c555f5ef08ab48b134560a36c7c9e0
ianzheng <flks_2005@qq.com>
2017-09-05 16:05:13 +0800

    add vue spa

--- js/app.js
+++ js/app.js
@@ -3,13 +3,19 @@
 const Home = { template: '<div>This is Home.</div>'}
 const Foo = { template: '<div>This is Foo.</div>'}
 const Bar = { template: '<div>This is Bar.</div>'}
+// const login = { template: "require('/template/login.html')"}
+
+var login = Vue.component('login', {
+    template: require('/templates/login.html')
+})
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
         {path: '/', name: 'home', component: Home},
         {path: '/foo', name: 'foo', component: Foo},
-        {path: '/bar', name: 'bar', component: Bar}
+        {path: '/bar', name: 'bar', component: Bar},
+        {path: '/login', name: 'login', component: login}
     ]
 })
 
@@ -23,6 +29,7 @@ new Vue({
                 <li><router-link :to="{ name: 'home' }">home</router-link></li>
                 <li><router-link :to="{ name: 'foo' }">foo</router-link></li>
                 <li><router-link :to="{ name: 'bar', params: { id: 123 }}">bar</router-link></li>
+                <li><router-link :to="{ name: 'login' }">login</router-link></li>
             </ul>
             <router-view class="view"></router-view>
         </div>
