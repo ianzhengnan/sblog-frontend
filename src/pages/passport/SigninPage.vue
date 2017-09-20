57cb269f671befe3c56e17f9c9e9e358245f94c0
ian zheng <flks_2005@qq.com>
2017-09-20 19:43:57 +0800

    modify sign in page

--- src/pages/passport/SigninPage.vue
+++ src/pages/passport/SigninPage.vue
@@ -1,13 +1,21 @@
 <template>
-	<form id="signin" v-on:submit.prevent="onSubmit">
-	    用户名: <input id="username" name="username" v-model="username"><br>
-	    密码: <input id="password" type="password" name="password" v-model="password"><br>
-	    <button>Sign in</button>
+	<form id="signin" class="form-signin" v-on:submit.prevent="onSubmit">
+		<h2 class="form-signin-heading">Please sign in</h2>
+		<label for="inputEmail" class="sr-only">Email address</label>
+    <input type="text" id="inputEmail" v-model="username" class="form-control" placeholder="User name" required autofocus>
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="inputPassword" class="form-control" v-model="password" placeholder="Password" required>
+    <div class="checkbox">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 	</form>
 </template>
 
 <script type="text/javascript">
-	
+
 
 export default {
 
@@ -60,4 +68,45 @@ export default {
 
 </script>
 
+<style media="screen">
+body {
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #eee;
+}
 
+.form-signin {
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .form-signin-heading,
+.form-signin .checkbox {
+  margin-bottom: 10px;
+}
+.form-signin .checkbox {
+  font-weight: normal;
+}
+.form-signin .form-control {
+  position: relative;
+  height: auto;
+  -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+          box-sizing: border-box;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
