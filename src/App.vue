1350edaf237426944bcdbb118f0962bcfe2cc768
ian zheng <flks_2005@qq.com>
2017-09-20 19:27:32 +0800

    switch to atom

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-	<div class='app'>
+	<div class='container'>
 		<div class="header">
-			<header-bar></header-bar>	
+			<header-bar></header-bar>
 		</div>
 		<div class="page">
 			<router-view></router-view>
 		</div>
 	</div>
 </template>
-
 
 <script type="text/javascript">
 	import HeaderBar from './components/Headerbar.vue'
